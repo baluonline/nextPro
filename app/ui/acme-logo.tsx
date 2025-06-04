714019af5a18ef99e7202b706c5f729b075ab353
baluonline <balurides@gmail.com>
2025-06-04 15:24:57 -0500

    next ui

---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,16 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+// import { lusitana } from '@/app/ui/fonts';
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className="flex flex-row items-center leading-none text-white"
+
+    >
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <h1 className="text-blue-500">I'm blue!</h1>
+    
+      <p className="text-[44px]">Acme</p>
+    </div>
+  );
+}
